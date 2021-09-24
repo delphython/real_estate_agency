4f6e53755f035e7e55b5015a631cb360535f6d93
delphython <delphython@gmail.com>
2021-09-24 14:48:22 +0300

    Add list_filter fields

--- property/admin.py
+++ property/admin.py
@@ -9,6 +9,6 @@ class FLatAdmin(admin.ModelAdmin):
     list_display = ('address', 'price', 'new_building', 'construction_year',
         'town',)
     list_editable = ('new_building',)
-
+    list_filter = ('new_building', 'rooms_number', 'has_balcony',)
 
 admin.site.register(Flat, FLatAdmin)
