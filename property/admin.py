bc7e4203875f0d6a799f146ca1c14184a707e109
delphython <delphython@gmail.com>
2021-09-24 14:09:26 +0300

    Add list editable fields

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class FLatAdmin(admin.ModelAdmin):
     readonly_fields = ["created_at",]
     list_display = ('address', 'price', 'new_building', 'construction_year',
         'town',)
+    list_editable = ('new_building',)
 
 
 admin.site.register(Flat, FLatAdmin)
