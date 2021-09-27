cdbce22d607e96be4bbacff89b838d01df7f4e71
delphython <delphython@gmail.com>
2021-09-27 14:51:58 +0300

    Add db_index=True to Owner

--- property/models.py
+++ property/models.py
@@ -75,18 +75,20 @@ class Complaint(models.Model):
         db_index=True)
 
     def __str__(self):
-        return 'Жалобы'
+        return f'Жалоба от {self.user}'
 
 
 class Owner(models.Model):
     flat = models.ManyToManyField(
         Flat,
         verbose_name='Квартиры в собственности',
-        related_name="flats")
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+        related_name="flats",
+        db_index=True)
+    owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20,
+        db_index=True)
     owner_pure_phone = PhoneNumberField('Нормализованный номер владельца',
-        null=True, blank=True,)
+        null=True, blank=True, db_index=True, )
 
     def __str__(self):
-        return 'Владелец'
+        return f'Владелец {self.owner}'
